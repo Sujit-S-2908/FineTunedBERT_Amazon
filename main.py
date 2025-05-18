6be8c961717b05e1f58ece55f8bb743de829b307
Sujit S <153498307+Sujit-S-2908@users.noreply.github.com>
2025-05-18 11:51:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-# Install dependencies as needed:
-!pip install kagglehub[pandas-datasets]
-
-# import kagglehub
-# from kagglehub import KaggleDatasetAdapter
-
-# # Set the path to the file you'd like to load
-# file_path = "./Reviews.csv"
-
-# # Load the latest version
-# df = kagglehub.load_dataset(
-#   KaggleDatasetAdapter.PANDAS,
-#   "snap/amazon-fine-food-reviews",
-#   file_path,
-#   # Provide any additional arguments like
-#   # sql_query or pandas_kwargs. See the
-#   # documenation for more information:
-#   # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
-# )
-
-# print("First 5 records:", df.head())
-
 import os
 import numpy as np
 import pandas as pd
